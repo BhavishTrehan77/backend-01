e024baa4a049e4d87b66f1fb02007f8b58b5a8f0
BhavishTrehan77 <bhavishtrehan777@gmail.com>
2026-05-13 11:47:29 +0530

    ...

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,5 @@
 const mongoose=require('mongoose')
 const bcrypt=require("bcrypt") 
-const { resetPassword } = require('../services/user.services')
 const Schemadata=new mongoose.Schema({
     name:{
         type:String
